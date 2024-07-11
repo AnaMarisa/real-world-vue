92f49c72c8e9ae3c190a97edb7f9a8dd26782a18
Ana Marisa <ana.marisa.mf@gmail.com>
2024-07-11 11:08:18 +0700

    flash message

--- src/main.js
+++ src/main.js
@@ -1,10 +1,15 @@
-import { createApp } from 'vue'
+import { createApp, reactive } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
+
+app.use(router)
+
+const GStore = reactive({ flashMessage: '' })
+app.provide('GStore', GStore)
 
 app.use(createPinia())
 app.use(router)
--- src/views/event/Register.vue
+++ src/views/event/Register.vue
@@ -1,11 +1,20 @@
 <script setup>
 import { useRouter } from 'vue-router'
+import { defineProps, inject } from 'vue'
+
 const props = defineProps(['event'])
 const router = useRouter()
+const GStore = inject('GStore')
+
 const register = () => {
-  // If registration successful
+  // Call to API
+  // If registered then redirect to event details
+  GStore.flashMessage = 'You are successfully registered for ' + props.event.title
+  setTimeout(() => {
+    GStore.flashMessage = ''
+  }, 3000)
   router.push({
-    name: 'EventDetails',
+    name: 'EventDetails'
   })
 }
 </script>
